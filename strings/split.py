91f174f56bcee8abd2cf7dc27714c1363558ad72
Mariomics <mariosb@lcg.unam.mx>
2025-03-21 13:11:25 -0600

    guardado

--- strings/split.py
+++ strings/split.py
@@ -4,7 +4,7 @@ print(list(map(int,numero_str)))
 lista_numeros = list(map(int,numero_str))
 
 suma = sum(lista_numeros)
-print(lista_numeros)
+print(suma)
 
 #num = [1, 2, 3]
 #suma_numeros = sum(num)
