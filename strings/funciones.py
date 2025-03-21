4d5fbff4ba381bc4eeed40d5b8daa028d12dd7eb
Mariomics <mariosb@lcg.unam.mx>
2025-03-21 13:48:57 -0600

    guardar cambios

--- strings/funciones.py
+++ strings/funciones.py
@@ -1,4 +1,13 @@
 secuencia = "GTACGATCA"
 for i, base in enumerate(secuencia): 
     print(f"posicion: {i} : {base}")
-    
+
+"""
+Funcion zip: itera sobre varias listas ----> xd 
+"""
+
+bases = "ATGC"
+complementos = "TACG"
+
+for base, complemento in zip (bases, complementos):
+    print(f"la base es {base} y su complemento es {complemento}")
