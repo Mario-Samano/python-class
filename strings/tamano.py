20a8ad141aec6c2c8a84410a7021744e9bd6f513
Mariomics <mariosb@lcg.unam.mx>
2025-03-21 13:31:22 -0600

    cambio guardado

--- strings/tamano.py
+++ strings/tamano.py
@@ -2,4 +2,4 @@ with open("genes.gff") as file:
     for linea in file: 
         # quitar los saltos de linea
         columna = linea.split().strip("\t")
-        print(columna)
+        print(columna[3])
