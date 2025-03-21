86aff6fefd471f2a48e368a684f36a9c125c904c
Mariomics <mariosb@lcg.unam.mx>
2025-03-21 13:08:11 -0600

    cambio guardado

--- strings/split.py
+++ strings/split.py
@@ -1,9 +1,10 @@
 numero_str = input("dame 3 numeros separados por un espacio:   ").split()
-print(list(map(int,numero_str)))
 print(numero_str)
-map(int)
-suma = sum(numero_str)
-print(suma)
+print(list(map(int,numero_str)))
+lista_numeros = list(map(int,numero_str))
+
+suma = sum(lista_numeros)
+print(lista_numeros)
 
 #num = [1, 2, 3]
 #suma_numeros = sum(num)
