5d02bea665eccc03ca4871a2ef6bca445cd65e45
Mariomics <mariosb@lcg.unam.mx>
2025-03-21 13:52:53 -0600

    GUARDADO

--- strings/funciones.py
+++ strings/funciones.py
@@ -5,9 +5,12 @@ for i, base in enumerate(secuencia):
 """
 Funcion zip: itera sobre varias listas ----> xd 
 """
-
 bases = "ATGC"
 complementos = "TACG"
-
+"""
+Cuando no son del mismo tamaño se toma el mas pequeño.
+AAAAA --> SE TOMA ESTA
+AAAAAA
+"""
 for base, complemento in zip (bases, complementos):
     print(f"la base es {base} y su complemento es {complemento}")
