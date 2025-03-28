55a75a7bcbbde2c858fe768813221f9de92a78ff
Mariomics <mariosb@lcg.unam.mx>
2025-03-28 13:07:04 -0600

    guardado

--- ejercicios/count_seq.py
+++ ejercicios/count_seq.py
@@ -3,4 +3,4 @@ with open(inputfile, "r") as infile:
     lineas = infile.readlines()
 
 lineas_filtradas = [linea for linea in lineas if linea.startswith(">")]
-print (f"Total de secuencias: {len(lineas_filtradas)}")
+print (f"Total de secuencias: {lineas_filtradas}")
