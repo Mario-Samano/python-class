c40ffa860b95f30c6e0a9713d1651393b590f15a
Mariomics <mariosb@lcg.unam.mx>
2025-03-28 13:03:31 -0600

    guardado

--- ejercicios/count_seq.py
+++ ejercicios/count_seq.py
@@ -2,3 +2,5 @@ inputfile = "secuencias.fa"
 with open(inputfile, "r") as infile:
     lineas = infile.readlines()
 print (lineas[0])
+print (lineas[1])
+[linea for linea in lineas if linea.startswith(">")]
