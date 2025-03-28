87693c51d284151bc65bfe6008d5e2c90c33f08e
Mariomics <mariosb@lcg.unam.mx>
2025-03-28 12:50:56 -0600

    guardado

--- ejercicios/ejercicio_5.py
+++ ejercicios/ejercicio_5.py
@@ -5,7 +5,6 @@ outputfile = "dna_sequences.fa"
 with open (inputfile, "r") as infile, open(outputfile, "w") as outfile:
     for linea in infile: 
         id, seq = linea.strip().split("\t")
-
         # ---------------------------------- mandar a un archivo de salida formato fasta 
         outputfile.write(f">{id}\n{seq.upper()}\n")
     
