787597865536a448fe9e526243a25a879ee2c60a
Mariomics <mariosb@lcg.unam.mx>
2025-03-28 12:47:22 -0600

    guardado 2

--- ejercicios/ejercicio_5.py
+++ ejercicios/ejercicio_5.py
@@ -2,7 +2,7 @@ inputfile = "dna_sequences.txt"
 outputfile = "dna_sequences.fa" 
 
 #------------------------------------------------------------------- leemos el archivo
-with open (inputfile, "r") as infile, open(outputfile) as outfile:
+with open (inputfile, "r") as infile, open(outputfile, "w") as outfile:
     for linea in infile: 
         id, seq = linea.strip().split("\t")
 
