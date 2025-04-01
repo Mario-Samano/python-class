2b26913dad4dba350805c6056b74be6914cd8a44
Mariomics <mariosb@lcg.unam.mx>
2025-03-31 23:27:18 -0600

    Archivo ejercicio_11.py guardado

--- ejercicios_clase/ejercicio_11.py
+++ ejercicios_clase/ejercicio_11.py
@@ -1,7 +1,5 @@
 secuencias = input("Ingrese las secuencias de ADN separadas por comas:\n").split(",")
 
-# -----------------------------------------------------------------Invertir cada secuencia
 secuencias_invertidas = [secuencia.strip()[::-1] for secuencia in secuencias]
 
-# Imprimir las secuencias invertidas
 print(f"Secuencias invertidas:\n {secuencias_invertidas}")
